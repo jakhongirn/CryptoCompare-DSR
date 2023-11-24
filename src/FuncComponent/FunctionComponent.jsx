3e09be09346ead100c6083ef288778072aa8bb63
Jakhongir <jahongirnagmatov@gmail.com>
2023-11-24 13:14:09 +0500

    update rates of list of cryptos

--- src/FuncComponent/FunctionComponent.jsx
+++ src/FuncComponent/FunctionComponent.jsx
@@ -4,25 +4,29 @@ import axios from 'axios';
 const API_KEY = '78aadbeda3580e6813514b8d58edd1c3d691c0496814b505d3d17a23692a710c';
 const CryptoCompare = () => {
 
-  const [crypto, setCrypto] = useState('btc')
+  const [cryptoList, setCryptoList] = useState(['doge', 'btc', 'eth'])
 
-  const [cryptoRate, setCryptoRate] = useState(null)
+  const [cryptoData, setCryptoData] = useState({})
 
   useEffect(() => {
    
-    fetchData()
-    const interval = setInterval(fetchData, 5000)
+    updateCryptoChanges()
+    const interval = setInterval(updateCryptoChanges, 5000)
     return () => clearInterval(interval)
   }, [])
 
   
-  const fetchData = () => {
-    axios
+  const updateCryptoChanges = () => {
+    cryptoList.forEach((crypto => {
+      axios
     .get(`https://min-api.cryptocompare.com/data/price?fsym=${crypto.toUpperCase()}&tsyms=USD&api_key=${API_KEY}`)
     .then(response => {
       const newData = response.data;
       if (newData && newData.USD) {
-        setCryptoRate(newData.USD);
+        setCryptoData((prevCryptoData) => ({
+          ...prevCryptoData,
+          [crypto]: newData.USD
+        }));
       }
       else {
         alert('Cryptocurrency not found')
@@ -31,12 +35,21 @@ const CryptoCompare = () => {
     .catch((error) => {
       console.error('Error fetching Dogecoin price:', error)
     })
+    }))
   }
+
+  const searchedCryptos = cryptoList.map((crypto) => {
+    return (
+      <li key={crypto}>
+        {crypto.toUpperCase()}: ${cryptoData[crypto]}
+      </li>
+    )
+  })
   
   return (
     <div>
 
-      <h1>{crypto}: {cryptoRate}</h1>
+      <ul>{searchedCryptos}</ul>
       
     </div>
   )
